94acef89e3d21be8872d938262de9d267935f46f
Home <ilyakeps@yndex.ru>
2025-08-12 17:59:16 +0300

    add 2 files

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-let one
+let element = document.getElementById("id");
+
+let elementByClassName = document.getElementsByClassName('.className');
+
+let  elementBaTag = document.getElementsByTagName('div');
+
+let elements = document.querySelectorAll('div');
+
+element.classList.add('newClassName');
+
+element.classList.remove('className');
+
+let bool = element.classList.contains('newClassName');
+if (bool) {
+    console.log('yes')
+}
+
+element.innerHTML = '<button>My_Button</button>';
+
+element.innerText = 'Моя кнопка';
+
+element.setAttribute('src', '../src/img.png');
+
+let atrButton = element.getAttribute('src');
+
+element.removeAttribute('src');
+
+element.addEventListener('click', (event) => {
+    console.log(event);
+});
+
+element.removeEventListener('click', (event) => {
+    console.log('клик удален');
+});
